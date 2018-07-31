cb26b3c4e1cc702cb1e2f0683063cbd30f8c02bc
李晓明 <xiao-ming.li1@reeed.org>
2018-07-31 10:59:45 +0800

    测试

--- src/FibonacciSequence.java
+++ src/FibonacciSequence.java
@@ -2,12 +2,15 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class FibonacciSequence {
-//    获取数组中最长裴波那切数列的长度
-    public static int length(int[] A) {
-        int N = length(A);
-        Set<Integer> s = new HashSet<>();
+    //    获取数组中最长裴波那切数列的长度
+    public int length(int[] A) {
+    //    方法记忆错误。。。默认想成数据库，获取表的长度，正好是length()。造成StackOverflowError
+        int N = A.length;
         int ans = 0;
+    //    初始化为set
+        Set<Integer> s = new HashSet<>();
         for (int x : A) s.add(x);
+
         for (int i = 0; i < N; i++) {
             for (int j = i + 1; j < N; j++) {
                 int x = A[j], y = A[i] + A[j];
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
+        int[] fibonacciTest = new int[]{1,2,3,4,5,6,7,8};
+        FibonacciSequence fibonacciSequence = new FibonacciSequence();
+        int length = fibonacciSequence.length(fibonacciTest);
 
-        System.out.println("Hello World!");
+        System.out.println("数组fibonacciTest中，最长裴波那切的长度为" + length);
+        System.out.println("呵呵");
     }
 }
